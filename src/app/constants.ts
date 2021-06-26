0c6ead5f54827bd617208d5d59f2ac995513bb93
leadream <leadream4@gmail.com>
2021-06-26 11:41:55 +0800

    add resolution explanation

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,17 +1,64 @@
 export const PLATFORMS = ['Web', 'iOS', 'Android'];
 
-export const WEB_MULTIPLE = ['1x', '2x', '3x'];
+export const WEB_MULTIPLE = [
+  {
+    label: '1x',
+    value: 1
+  },
+  {
+    label: '2x',
+    value: 2
+  },
+  {
+    label: '3x',
+    value: 3
+  }
+];
 
-export const IOS_DENSITY = ['@1x', '@2x', '@3x'];
+export const IOS_DENSITY = [
+  {
+    label: '@1x',
+    value: 1
+  },
+  {
+    label: '@2x',
+    value: 1 / 2
+  },
+  {
+    label: '@3x',
+    value: 1 / 3
+  }
+];
 
 export const ANDROID_DENSITY = [
-  'ldpi (4/3)',
-  'mdpi',
-  'tvdpi (3/4)',
-  'hdpi (2/3)',
-  'xhdpi (1/2)',
-  'xxhdpi (1/3)',
-  'xxxhdpi (1/4)'
+  {
+    label: 'ldpi (4/3)',
+    value: 4 / 3
+  },
+  {
+    label: 'mdpi',
+    value: 1
+  },
+  {
+    label: 'tvdpi (3/4)',
+    value: 3 / 4
+  },
+  {
+    label: 'hdpi (2/3)',
+    value: 2 / 3
+  },
+  {
+    label: 'xhdpi (1/2)',
+    value: 1 / 2
+  },
+  {
+    label: 'xxhdpi (1/3)',
+    value: 1 / 3
+  },
+  {
+    label: 'xxxhdpi (1/4)',
+    value: 1 / 4
+  }
 ];
 
 export const UNITS = ['pt', 'dp', 'px', 'em', 'rem', 'rpx'];
--- src/app/pages/Settings/Mark.tsx
+++ src/app/pages/Settings/Mark.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {withGlobalContextConsumer, withTranslation} from '@app/context';
+import {Docs} from '@components/icons';
 import {changeSettingsAndNotifyBackground} from '@utils/helper';
 import {PLATFORMS, WEB_MULTIPLE, IOS_DENSITY, ANDROID_DENSITY, UNITS} from '@app/constants';
 
@@ -14,28 +15,37 @@ const Mark = ({globalData, changeGlobalData, t}) => {
   const {platform, resolution, unit, remBase} = globalData;
   const baseVisible = platform === 0 && (unit === 3 || unit === 4);
   const units = getUnits(platform);
-  const handleChange = e => {
+
+  function handleChange(e) {
     const {name, value} = e.target;
     const changedSettings: any = {};
     if (name === 'platform') {
       const currentPlatform = value - 0;
       const unitRange = unitMaps[currentPlatform];
-      const resolutionRange = resolutions[currentPlatform];
       if (unitRange.indexOf(unit) < 0) {
         // unit = unitRange[0]
         changeGlobalData('unit', unitRange[0]);
         changedSettings.unit = unitRange[0];
       }
-      if (!resolutionRange[resolution]) {
-        // resolution = 0
-        changeGlobalData('resolution', 0);
-        changedSettings.resolution = 0;
-      }
+      resetResolution(currentPlatform, changedSettings);
     }
     changedSettings[name] = Math.floor(value - 0); // avoid decimals when remBase
     changeGlobalData(changedSettings);
     changeSettingsAndNotifyBackground(globalData, changedSettings);
-  };
+  }
+
+  function resetResolution(platform, changedSettings) {
+    const defaultResolution = platform === 2 ? 1 : 0;
+    changeGlobalData('resolution', defaultResolution);
+    changedSettings.resolution = defaultResolution;
+  }
+
+  function getMarkSize() {
+    const scale = resolutions[platform][resolution].value;
+    const num = (80 * scale) / (baseVisible ? remBase : 1);
+    return (num === Math.floor(num) ? num : num.toFixed(2)) + UNITS[unit];
+  }
+
   return (
     <div className="settings-block">
       <div className="section-title type type--pos-medium-bold">{t('mark')}</div>
@@ -54,10 +64,21 @@ const Mark = ({globalData, changeGlobalData, t}) => {
         <select name="resolution" className="select" value={resolution} onChange={handleChange}>
           {resolutions[platform].map((resolution, index) => (
             <option value={index} key={index}>
-              {resolution}
+              {resolution.label}
             </option>
           ))}
         </select>
+      </div>
+      <div className="form-item form-item-helper">
+        <label className="label" />
+        <a
+          className="helper"
+          href="https://docs.heron.design/designer/#%E4%B8%BA%E4%BB%80%E4%B9%88%E6%88%91%E9%80%89%E4%BA%86-2x-%E6%A0%87%E6%B3%A8%E5%8F%98%E6%88%90%E4%B8%80%E5%8D%8A%E4%BA%86"
+          target="_blank"
+        >
+          {t('resolution explanation').replace('{num}', getMarkSize())}
+          <Docs size={12} />
+        </a>
       </div>
       <div className="form-item">
         <label className="label">{t('unit')}</label>
